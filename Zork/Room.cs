7ba24a70df6f0a815cabcc4581c7ab74e076d9e8
rossbrinkman <55204801+rossbrinkman@users.noreply.github.com>
2021-09-15 15:10:54 -0400

    Zork 3.1 (In Progress)

---
+++ Zork/Room.cs
@@ -0,0 +1,16 @@
+﻿
+namespace Zork
+{
+    public class Room
+    {
+        public string Name { get; }
+
+        public string Description { get; set; }
+
+        public Room(string name, string description = "")
+        {
+            Name = name;
+            Description = description;
+        }
+    }
+}
