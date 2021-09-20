c987408b5eacb1f5acb22d3e67034d6604ba358a
rossbrinkman <55204801+rossbrinkman@users.noreply.github.com>
2021-09-20 14:48:07 -0400

    Zork 3.1 (In progress)
    Added new Assert class file

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -1,19 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 
 namespace Zork
 {
-    public static class Assert
-    {
-        [Conditional("DEBUG")]
-        public static void IsTrue(bool expression, string message = null)
-        {
-            if (expression == false)
-                throw new Exception(message);
-        }
-    }
-
     internal class Program
     {
         private static string CurrentRoom
